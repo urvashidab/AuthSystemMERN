08a47ed6c8a578181ec0624009ddba0a806af191
urvashidab <urvashi.dabgotra11@gmail.com>
2026-04-09 16:45:26 +0530

    frontend colors selected and navbar completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,37 @@
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ResetPassword from "./pages/ResetPassword";
+import About from "./pages/About";
 
 const App = () => {
-  return <div className="bg-amber-400">hello</div>;
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-1">
+        <ToastContainer position="top-right" autoClose={3000} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/resetpassword" element={<ResetPassword />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="py-10 flex justify-between ">
+      {/* right side title */}
+      <h1 className="text-headingText tracking-wider">Secure Auth API</h1>
+
+      {/* middle part */}
+      <ul className="lg:flex gap-6 items-center justify-center hidden">
+        <NavLink className="nav-link" to="/">
+          Home
+        </NavLink>
+        <NavLink className="nav-link" to="/about">
+          about
+        </NavLink>
+        <NavLink className="nav-link" to="/contact">
+          contact
+        </NavLink>
+
+        <NavLink className="nav-link font-extrabold  " to="/contact">
+          admin log in
+        </NavLink>
+      </ul>
+
+      {/* right side-login */}
+
+      <NavLink to="/login" className="btn px-6 py-1.5 rounded">
+        log in
+      </NavLink>
+    </div>
+  );
+};
+
+export default Navbar;
